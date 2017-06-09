60b2cd976b4fe1155d6d698b58cf600239f422ad
Camilla Kümpel <camilla.kumpel@gmail.com>
2017-06-09 17:43:07 +0200

    Removed auto update on slug field. Added region/country dependency and added buttons for manual and safe slug generation.

--- samples/slug/README.md
+++ samples/slug/README.md
@@ -1,45 +1,59 @@
-Slug Generator
+Updated Slug Generator
 --------------
 
-![slug-widget](http://contentful.github.io/extensions/assets/slug-widget.png)
+![slug-widget](http://i.imgur.com/zXnPjca.png)
 
-This extension enables to generate a slug using the title of the entry.
+![slug-widget](http://i.imgur.com/TClQNg2.png)
+
+###Updated Contentful UI Extension.
+
+Slug is no longer auto generated (which was dangerous, as unexpected behavior could occur).
+
+* Added "Get URL from field"-button that fetches the data from either `Title` or `Heading` fields.
+* Added "Manually generate URL"-button that generates url from input field. Is Swedish/Norweigan/Danish adapted to translate `å/Å -> a` `ä/Ä -> a` `æ/Æ -> a` `ö/Ö -> o` `ø/Ø -> o`. Other URL behavior is from [speakurl](https://github.com/pid/speakingurl).
+
+**Instead of using contentfuls unique requirement, the extension now checks all content types (that has a slug field) for duplicates but only content that has the region field value (to allow same urls in different languages).**
+
+~~This extension enables to generate a slug using the title of the entry.
 It also checks whether or not any duplicates across the other entries of the
-same content type exist to warn users before publishing.
+same content type exist to warn users before publishing.~~
 
-**Note**: In order to prevent publication whenever another entry has the same
+~~Note In order to prevent publication whenever another entry has the same
 value for the field, it is advised to use the "Uniqueness" validation constraint.
-It can be combined with this extension.
+It can be combined with this extension.~~
 
 ### Installation steps
 
-Ensure you checked [the samples requirements listed here](../README.md).
+~~Ensure you checked the samples requirements listed here.~~
 
 Install dependencies if not done already through `npm install`.
 
-You then need to update the [Makefile](./Makefile), uncomment and update the
-following lines:
-```Makefile
-export CONTENTFUL_MANAGEMENT_ACCESS_TOKEN=<your token>
-export SPACE=<id of space you want to install the extension for>
-```
+Since makefile doesnt work on windows, betterScripts was added to package.json to handle creation and update. See the extension.json for required file for create/update.
 
-Then do the following:
-```bash
-make create
+###Creating and updating (after concatinating the file and inputting your space and AT), is done by:
+
+```
+> npm run create
 ```
 
 ### Updating the extension
-If you make any changes to the code of the extension, you may push these updates
-to Contentful by calling:
-```bash
-make update
+```
+> npm run create
 ```
 
-### Debugging in local environment
+~~You then need to update the Makefile uncomment and update the
+following lines:
+export CONTENTFUL_MANAGEMENT_ACCESS_TOKEN=<your token>
+export SPACE=<id of space you want to install the extension for>
+Then do the following:
+bash
+make create
+Updating the extension
+If you make any changes to the code of the extension, you may push these updates
+to Contentful by calling:
+make update
+Debugging in local environment
 If you wish to run the extension from your local environment for debugging
 purposes, you may use the following commands:
-```bash
 make update-dev
-make serve
-```
+make serve~~
--- samples/slug/app.js
+++ samples/slug/app.js
@@ -1,75 +1,183 @@
-// import { init } from '../../lib/api'
-const cfExt = window.contentfulExtension || window.contentfulWidget
-
+const cfExt = window.contentfulExtension || window.contentfulWidget;
 
 cfExt.init(api => {
-  const slugField = api.field
-  const titleField = api.entry.fields.title
+    const slugField = api.field;
 
-  const _ = window._
-  const getSlug = window.getSlug
-  const debouncedUpdateStatus = _.debounce(updateStatus, 500)
+    // if title doesnt exist, fallback to field heading
+    const siblingField = api.entry.fields.title || api.entry.fields.heading;
+    const currentRegion = api.entry.fields.region;
 
-  const input = document.getElementById('slug')
-  const statusElements = {
-    error: document.getElementById('error'),
-    ok: document.getElementById('ok'),
-    loading: document.getElementById('loading')
-  }
+    const warningMessage = document.getElementById('warningMessage');
+    const slug = document.getElementById('slug');
+    const slugGenerated = document.getElementById('slugGenerated');
+    const makeSlug = document.getElementById('makeSlug');
+    const generateSlug = document.getElementById('generateSlug');
+    const duplicate = document.getElementById('duplicate');
 
-  api.window.updateHeight()
+    const _ = window._;
+    const debouncedUpdateStatus = _.debounce(updateStatus, 500);
+    const getSlug = window.getSlug;
+    const options = {
+        custom: {
+            "å": "a",
+            "Å": "a",
+            "ä": "a",
+            "Ä": "a",
+            "æ": "a",
+            "Æ": "a",
+            "ö": "o",
+            "Ö": "o",
+            "ø": "o",
+            "Ø": "o"
+        }
+    }
 
-  titleField.onValueChanged(handleSlugChange)
+    const statusElements = {
+        error: document.getElementById('error'),
+        ok: document.getElementById('ok'),
+        loading: document.getElementById('loading')
+    }
 
-  input.addEventListener('input', () => handleSlugChange(input.value))
-  input.addEventListener('change', () => handleSlugChange(input.value))
+    // ??
+    api.window.startAutoResizer()
 
-  updateStatus(slugField.getValue())
+    // Populate input with existing field value
+    slugGenerated.value = slugField.getValue() || "";
+
+    // Events to generate and make slug
+    makeSlug.addEventListener('click', () => manualSlugCreate() );
+    generateSlug.addEventListener('click', () => generateSlugFromField() );
+
+
+    updateStatus(slugField.getValue())
+
+    /**
+    * Event to generate slug from field
+    * Generatint from title or heading field
+    */
+    function generateSlugFromField () {
+        warningMessage.style.display = "none";
+
+        if(!currentRegion.getValue()){
+            warningMessage.style.display = "block";
+            warningMessage.innerText = 'Set the country before getting the slug!';
+            setStatus('error');
+
+            api.field.setInvalid(true);
+
+            return;
+
+        } else if(!siblingField.getValue()){
+            warningMessage.style.display = "block";
+            warningMessage.innerText = 'Title or heading is required to get URL!';
+            setStatus('error');
+
+            api.field.setInvalid(true);
+
+            return;
+
+        } else {
+            slugGenerated.value = getSlug(siblingField.getValue(), options)
+            handleSlugChange(slugGenerated.value);
+        }
+    }
+
+    function manualSlugCreate() {
+        warningMessage.style.display = "none";
+
+        if(!currentRegion.getValue()){
+            warningMessage.style.display = "block";
+            warningMessage.innerText = 'Set the country before setting the slug!';
+            setStatus('error');
+
+            api.field.setInvalid(true);
+
+            return;
+
+        } else if(!slug.value){
+            warningMessage.style.display = "block";
+            warningMessage.innerText = 'Input is required to generate URL!';
+            setStatus('error');
+
+            api.field.setInvalid(true);
+
+            return;
+
+        } else {
+
+            slugGenerated.value = getSlug(slug.value, options);
+            handleSlugChange(slugGenerated.value);
+
+            slug.value = "";
+        }
+    }
 
   /**
-   * Handle change of slug value caused by either changing slug field
-   * or changing the title of the entry
+   * Handle change of slug value caused by either typing and generating a value
+   * or generating from field
    */
-  function handleSlugChange (value) {
-    setSlug(getSlug(value || ''))
-  }
+    function handleSlugChange (value) {
+        warningMessage.style.display = "none";
 
-  /**
-   * Set the input value to 'slug' and update the status by checking for
-   * duplicates.
-   */
-  function setSlug (slug) {
-    input.value = slug
-    slugField.setValue(slug)
-    setStatus('loading')
-    debouncedUpdateStatus(slug)
-  }
+        setSlug(getSlug(value || ''), options);
+    }
 
-  /**
-   * Show inline status icon based on current status
-   */
-  function updateStatus (slug) {
-    getDuplicates(slug).then(function (hasDuplicates) {
-      if (hasDuplicates) {
-        setStatus('error')
-      } else {
-        setStatus('ok')
-      }
-    })
-  }
+    /**
+    * Set the input value to 'slug' and update the status by checking for
+    * duplicates.
+    */
+    function setSlug (slug) {
+        slugGenerated.value = slug;
+        slugField.setValue(slug);
+        setStatus('loading');
+        debouncedUpdateStatus(slug);
+    }
 
-  /**
-   * Show icon for given status
-   */
-  function setStatus (status) {
-    _.each(statusElements, function (el, name) {
-      if (name === status) {
-        el.style.display = 'inline'
-      } else {
-        el.style.display = 'none'
-      }
-    })
-  }
+    /**
+    * Show inline status icon based on current status
+    */
+    function updateStatus (slug) {
+
+        getDuplicates(slug)
+        .then(function (res) {
+
+            if (res.hasDuplicates && slug) {
+                warningMessage.style.display = "block";
+                warningMessage.innerText = 'A page with this URL already exists!!!';
+                setStatus('error');
+
+                api.field.setInvalid(true);
+
+                duplicate.style.display = "block";
+
+                for(var dup of res.dupliactes) {
+
+                    duplicate.innerHTML += '<li class="duplicate-item"><a href="/spaces/' + dup.sys.space.sys.id  + '/entries/' + dup.sys.id + '" target="_blank">'  +  dup.fields.name['sv-SE'] + ' <i class="fa fa-external-link"></i></a></li>';
+                }
+
+            } else if(slug) {
+                warningMessage.style.display = "none";
+                setStatus('ok');
+
+                api.field.setInvalid(false);
+            } else {
+                console.log("Something went wrong.");
+            }
+        })
+    }
+
+    /**
+    * Show icon for given status
+    */
+    function setStatus (status) {
+        _.each(statusElements, function (el, name) {
+            if (name === status) {
+                el.style.display = 'inline-block';
+            } else {
+                el.style.display = 'none';
+            }
+        })
+    }
 
 
   /**
@@ -77,18 +185,24 @@ cfExt.init(api => {
    * Resolves to 'true' if there are entries of the given content type that have
    * the same 'slug' value.
    */
-  function getDuplicates (slug) {
-    if (!slug) {
-      return Promise.resolve(false)
+    function getDuplicates (slug) {
+        if (!slug) {
+           return Promise.resolve(false);
+        }
+
+        return api.space.getEntries({
+            'query': slug
+        }).then(res => {
+
+            let items = res.items.filter(item => {
+
+                // If its not the current page, if the current region matches and if the item has a navigationName field
+                if(item.sys.id != api.entry.getSys().id && (item.fields.region && currentRegion.getValue() == item.fields.region['sv-SE']) && (item.fields.navigationName && item.fields.navigationName['sv-SE'] == slug)){
+                    return item;
+                }
+            })
+
+            return { hasDuplicates: items.length > 0, dupliactes: items };
+        });
     }
-
-    let query = {}
-
-    query['content_type'] = api.entry.getSys().contentType.sys.id
-    query['fields.' + slugField.id] = slug
-    query['sys.id[ne]'] = api.entry.getSys().id
-    query['sys.publishedAt[exists]'] = true
-
-    return api.space.getEntries(query).then(result => result.total > 0)
-  }
-})
+});
